b089b00375b95b658c29266d9d3fa7974224cab4
Juliana Arrighi <jarrighi@poli.ufrj.br>
2023-09-01 16:12:03 +0200

    Add test for /products.

--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe "Products", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET /products" do
+    it 'get index and return 200 OK' do
+      get products_path
+      expect(response).to have_http_status(200)
+    end
   end
 end
