c9e7dbf50076de73a539cbd4ec43aa633b270489
Juliana Arrighi <jarrighi@poli.ufrj.br>
2023-09-01 16:05:24 +0200

    Create tests for model Product.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
+  validates :product_type, :price, :name, presence: true
   enum product_type: {
     product: 0,
     event: 1
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations with shouda matchers' do
+    it { is_expected.to validate_presence_of(:product_type) }
+    it { is_expected.to validate_presence_of(:price) }
+    it { is_expected.to validate_presence_of(:name) }
+  end
 end
