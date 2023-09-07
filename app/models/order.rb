aa19309885d0fc5721608e522fc04307a4bf409c
Juliana Arrighi <jarrighi@poli.ufrj.br>
2023-09-07 19:57:26 +0200

    Add thumbnails to products.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,9 +1,8 @@
 class Order < ApplicationRecord
   belongs_to :cart, optional: true
   belongs_to :product
-  # belongs_to :product
-  # accepts_nested_attributes_for :products
-  def as_json(option={})
+
+  def as_json(_option)
     super(
       root: true,
       include: [:product]
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,20 @@
 class Product < ApplicationRecord
   validates :product_type, :price, :name, presence: true
-  # belongs_to :order
   has_one_attached :thumbnail
 
   enum product_type: {
     product: 0,
     event: 1
   }
+
+  def thumbnail_url
+    Faker::LoremFlickr.image  #(size: "50x60")
+  end
+
+  def as_json(_option)
+    super(
+      root: true,
+      methods: :thumbnail_url
+    )
+  end
 end
