c5ccd291a38fe67005512428e5b9b174bfa7ba6d
Juliana Arrighi <jarrighi@poli.ufrj.br>
2023-09-10 22:19:17 +0200

    Fix update

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -76,10 +76,10 @@ class OrdersController < ApplicationController
     product = Product.find(@order[:product_id])
     stock = product.stock
     wish = order_params[:quantity]
-    if @order.quantity < wish && (stock - wish).positive?
+    if product.stock < wish && (stock - wish).positive?
       product[:stock] = stock - wish
       product.save
-    elsif @order.quantity > wish
+    elsif product.stock > wish
       product[:stock] = stock + (@order.quantity - wish)
       product.save
     else
